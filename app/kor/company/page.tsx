d9fefcffa0026dc2318317961ed7ec3fc31b3628
jun_ <gurwns232@naver.com>
2024-04-15 14:00:26 +0900

    fixed: add suspense

--- app/kor/company/page.tsx
+++ app/kor/company/page.tsx
@@ -3,24 +3,26 @@ import ArtistSection from '@/app/components/ArtistSection'
 import BusinessSection from '@/app/components/BusinessSection'
 import CompanySection from '@/app/components/CompanySection'
 import InfoSection from '@/app/components/InfoSection'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const page = () => {
   return (
     <div className="relative w-full h-full">
-      <Animate>
-        <>
-          <CompanySection pathString="info">
-            <InfoSection />
-          </CompanySection>
-          <CompanySection pathString="artist">
-            <ArtistSection />
-          </CompanySection>
-          <CompanySection pathString="business">
-            <BusinessSection />
-          </CompanySection>
-        </>
-      </Animate>
+      <Suspense>
+        <Animate>
+          <>
+            <CompanySection pathString="info">
+              <InfoSection />
+            </CompanySection>
+            <CompanySection pathString="artist">
+              <ArtistSection />
+            </CompanySection>
+            <CompanySection pathString="business">
+              <BusinessSection />
+            </CompanySection>
+          </>
+        </Animate>
+      </Suspense>
     </div>
   )
 }
